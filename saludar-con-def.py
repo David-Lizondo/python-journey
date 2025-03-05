32ca1101a080fcbbcb8a19a785947e0ab3ea0eb7
davidLizondo <licdavidlizondo@gmail.com>
2025-03-05 02:21:55 -0300

    se ha creado saludar-con-def.py donde se trabaja con una funcion simple

---
+++ saludar-con-def.py
@@ -0,0 +1,10 @@
+def saluda(nombre:str) -> None:
+    print("Hola, " + nombre)
+    print("Como estas?")
+    print("Hasta luego!")
+    print("Adios " + nombre + " gracias por saludarme!")
+    print(" ");
+
+saluda("Gustavo")
+saluda("Juan")
+saluda("Maria")
